b57a245f25d9a5e2e81cddb4a860a996d366aaf0
Hassan Khan <hassan@expo.dev>
2026-04-26 02:44:45 +0100

    fix(expo-font): align `crossOrigin` type with React

--- packages/expo-font/src/Font.types.ts
+++ packages/expo-font/src/Font.types.ts
@@ -88,7 +88,7 @@ export type ServerFontResourceDescriptor =
   | {
       type: 'link';
       as: 'font';
-      crossOrigin?: string;
+      crossOrigin?: 'anonymous' | 'use-credentials' | '' | undefined;
       href: string;
       rel: 'preload';
     };
